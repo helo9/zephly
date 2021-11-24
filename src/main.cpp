605e9ef2c3c12eed2de3e2f992bb761b885dba4f
Jonathan Hahn <helo9@users.noreply.github.com>
2021-11-24 17:07:53 +0100

    Implement Sprektrum SRXL2 protocol

--- src/main.cpp
+++ src/main.cpp
@@ -7,46 +7,58 @@
 
 #include <attitude_estimation.hpp>
 #include "pwm.hpp"
+#include "rc/rc.hpp"
 
 constexpr double DEG2RAD = 0.017453292519943295;
 
 using namespace AttitudeEstimation;
 
 const float outputs[] = {0.5f, 0.5f, 0.5f, 0.5f};
+RCInput rc_input;
 
 void report_state(const Quaternion<double> &q, const Vector<double, 3> &w, const Vector<double, 3> &a);
 
 int main() {
 
-  if (!initialize_sensors() || !pwm().is_ready()) {
-    printk("Initialization failed!");
-    return -1;
-  }
+	if(rc_init() < 0) {
+	printk("RC Initialization failed!\n");
+	return -1;
+	}
 
-  pwm().write(outputs);
+	if (!initialize_sensors() || !pwm().is_ready()) {
+	printk("Initialization failed!");
+	return -1;
+	}
 
-  /* initial attitude */
-  Quaternion<double> q = {1.0, 0.0, 0.0, 0.0};
+	pwm().write(outputs);
 
-  while (true) {
-    if (!update_measurements()) {
-      /* Something went wrong, try again */
-      printf("Update failed!");
-      continue;
-    }
+	/* initial attitude */
+	Quaternion<double> q = {1.0, 0.0, 0.0, 0.0};
 
-    propagate_attitude<double>(q, get_rotation_speed(), get_delta_t());
+	rc_run();
 
-    report_state(q, get_rotation_speed(), get_accelerations());
+	while (true) {
 
-    k_sleep(K_MSEC(50));
-  }
+		rc_input = rc_get();
 
-  return 0;
+		if (!update_measurements()) {
+			/* Something went wrong, try again */
+			printf("Update failed!");
+			continue;
+		}
+
+		propagate_attitude<double>(q, get_rotation_speed(), get_delta_t());
+
+		report_state(q, get_rotation_speed(), get_accelerations());
+
+		k_sleep(K_MSEC(500));
+	}
+
+	return 0;
 }
 
 void report_state(const Quaternion<double> &q, const Vector<double, 3> &w, const Vector<double, 3> &a) {
-  printf("%f %f %f %f", q.w, q.x, q.y, q.z);
-  printf(" %u %f %f %f", k_uptime_get_32(), w.data[0], w.data[1], w.data[2]);
-  printf(" %f %f %f \n", a.data[0], a.data[1], a.data[2]);
+	printf("%f %f %f %f", q.w, q.x, q.y, q.z);
+	printf(" %u %f %f %f", k_uptime_get_32(), w.data[0], w.data[1], w.data[2]);
+	printf(" %f %f %f \n", a.data[0], a.data[1], a.data[2]);
 }
