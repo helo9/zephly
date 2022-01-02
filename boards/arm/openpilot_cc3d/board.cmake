625cf1991fcf42e5a7d5a2d0d76ed02f14a293e6
Jonathan Hahn <helo9@users.noreply.github.com>
2022-01-02 13:55:02 +0100

    Fix west flash for cc3d

--- boards/arm/openpilot_cc3d/board.cmake
+++ boards/arm/openpilot_cc3d/board.cmake
@@ -1,6 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(jlink "--device=STM32F103RC" "--speed=4000")
+board_runner_args(jlink "--device=STM32F103CB" "--speed=4000")
 
-include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
