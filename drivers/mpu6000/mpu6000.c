97205142321f967e02e837d81ff971e2a2e87bcf
Jonathan Hahn <helo9@users.noreply.github.com>
2022-01-04 13:38:35 +0100

    drivers: mpu6000: add wakeup

--- drivers/mpu6000/mpu6000.c
+++ drivers/mpu6000/mpu6000.c
@@ -5,23 +5,41 @@
 
 #define DT_DRV_COMPAT invensense_mpu6000
 
-#define MPU6000_SPI_READ_FLAG (1<<7)
+#define MPU6000_SPI_READ_FLAG   BIT(7)
 
-#define MPU6000_REG_WHO_AM_I    0x75
-#define MPU6000_WHO_AM_I        0x68
+#define MPU6000_REG_DATA_START	0x3BU
+
+#define MPU6000_REG_PWR_MGMT1	0x6BU
+#define MPU6000_SLEEP_EN		BIT(6)
+
+#define MPU6000_REG_WHO_AM_I    0x75U
+#define MPU6000_WHO_AM_I        0x68U
+
+
 
 #include <init.h>
 #include <device.h>
 #include <devicetree.h>
 #include <drivers/spi.h>
 #include <drivers/sensor.h>
+#include <sys/byteorder.h>
 
 struct mpu6000_config {
     struct spi_dt_spec spi;
 };
 
 struct mpu6000_data {
-    int nix2;
+    int16_t accel_x;
+	int16_t accel_y;
+	int16_t accel_z;
+	uint16_t accel_sensitivity_shift;
+
+	int16_t temp;
+
+	int16_t gyro_x;
+	int16_t gyro_y;
+	int16_t gyro_z;
+	uint16_t gyro_sensitivity_x10;
 };
 
 static int mpu6000_spi_read(const struct device *dev, uint8_t reg,
@@ -60,6 +78,33 @@ static int mpu6000_spi_read(const struct device *dev, uint8_t reg,
 	return 0;
 }
 
+static int mpu6000_spi_write(const struct device *dev, uint8_t reg,
+                    uint8_t *data, uint16_t len) {
+	const struct mpu6000_config *config = dev->config;
+	uint8_t buffer_tx = reg;
+	const struct spi_buf tx_buf[2] = {
+		{
+			.buf = &buffer_tx,
+			.len = 1,
+		},
+		{
+			.buf = data,
+			.len = len,
+		}
+	};
+	const struct spi_buf_set tx = {
+		.buffers = tx_buf,
+		.count = 2,
+	};
+
+	int ret = spi_write_dt(&config->spi, &tx);
+	if (ret < 0) {
+		return ret;
+	}
+
+    return 0;
+}
+
 static int mpu6000_init(const struct device *dev) {
     const struct mpu6000_config *config = dev->config;
 
@@ -69,16 +114,22 @@ static int mpu6000_init(const struct device *dev) {
     }
 
     uint8_t wai = 0;
-    //mpu6000_spi_read(dev, MPU6000_REG_WHO_AM_I, &wai, 1);    
     int ret = mpu6000_spi_read(dev, MPU6000_REG_WHO_AM_I, &wai, 1);
     if ( ret != 0 ) {
         printk("WAI request failed\n");
         return ret;
     }
 
+    /* check who am i register for correct value */
     if ( wai != MPU6000_WHO_AM_I ) {
         printk("WAI is wrong %d", wai);
         return -ENODEV;
+    }
+
+    /* wake up device */
+    ret = mpu6000_spi_write(dev, MPU6000_REG_PWR_MGMT1, MPU6000_SLEEP_EN, 1);
+    if ( ret != 0 ) {
+        return ret;
     }
 
     return 0;
@@ -93,6 +144,26 @@ static int mpu6000_set(const struct device *dev,
 
 static int mpu6000_fetch(const struct device *dev,
                         enum sensor_channel chan) {
+    struct mpu6000_data *data = dev->data;
+
+    if ((chan != SENSOR_CHAN_ALL) && (chan != SENSOR_CHAN_GYRO_XYZ)) {
+        return -ENOTSUP;
+    }
+    int16_t buf[7];
+
+    int ret = mpu6000_spi_read(dev, MPU6000_REG_DATA_START, (uint8_t*)buf,  14);
+    if ( ret != 0 ) {
+        return ret;
+    }
+
+    data->accel_x = sys_be16_to_cpu(buf[0]);
+	data->accel_y = sys_be16_to_cpu(buf[1]);
+	data->accel_z = sys_be16_to_cpu(buf[2]);
+	data->temp = sys_be16_to_cpu(buf[3]);
+	data->gyro_x = sys_be16_to_cpu(buf[4]);
+	data->gyro_y = sys_be16_to_cpu(buf[5]);
+	data->gyro_z = sys_be16_to_cpu(buf[6]);
+    
     return 0;
 }
 
