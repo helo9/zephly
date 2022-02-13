174641fcaeacdad6423f766232723075e31d11c2
Jonathan Hahn <helo9@users.noreply.github.com>
2022-02-13 15:01:20 +0100

    lib: mixer: add anti-windup backcalc support

--- lib/mixer/mixer.cpp
+++ lib/mixer/mixer.cpp
@@ -3,17 +3,24 @@
 
 #define FLT_EPSILON __FLT_EPSILON__
 
-/* the motor matrix inverse in transposed form */
-static constexpr float B_inv_transposed[4][4] = {
-    { 1.f,  1.f, -1.f, -1.f},
-    { 1.f, -1.f, -1.f,  1.f},
-    {-1.f,  1.f, -1.f,  1.f},
-    { 1.f,  1.f,  1.f,  1.f}
+/* the motor matrix and inverse in transposed form */
+static constexpr float motor_matrix_transposed[4][4] = {
+	{ 0.250f,  0.250f, -0.250f,  0.250f},
+	{ 0.250f, -0.250f,  0.250f,  0.250f},
+	{-0.250f, -0.250f, -0.250f,  0.250f},
+	{-0.250f,  0.250f,  0.250f,  0.250f}
+};
+
+static constexpr float motor_matrix_inverse_transposed[4][4] = {
+	{ 1.000f,  1.000f, -1.000f, -1.000f},
+	{ 1.000f, -1.000f, -1.000f,  1.000f},
+	{-1.000f,  1.000f, -1.000f,  1.000f},
+	{ 1.000f,  1.000f,  1.000f,  1.000f}
 };
 
 /* yaw and thrust multipliers used for desaturation in airmode mixer */
-static constexpr const float (&yaw_scales)[4] = B_inv_transposed[2];
-static constexpr const float (&thrust_scales)[4] = B_inv_transposed[3];
+static constexpr const float (&yaw_scales)[4] = motor_matrix_inverse_transposed[2];
+static constexpr const float (&thrust_scales)[4] = motor_matrix_inverse_transposed[3];
 
 /**
  * @brief Get the value of the motor matrix inverse with none transposed indices
@@ -22,8 +29,19 @@ static constexpr const float (&thrust_scales)[4] = B_inv_transposed[3];
  * @param j column of motor matrix inverse
  * @return constexpr float the value at the given indices
  */
-static constexpr float get_b_inv_val(int i, int j) {
-    return B_inv_transposed[j][i];
+static constexpr float get_motor_matrix_inv_val(int i, int j) {
+    return motor_matrix_inverse_transposed[j][i];
+}
+
+/**
+ * @brief Get the value of the motor matrix with none transposed indices
+ * 
+ * @param i row of motor matrix inverse
+ * @param j column of motor matrix inverse
+ * @return constexpr float the value at the given indices
+ */
+static constexpr float get_motor_matrix_val(int i, int j) {
+    return motor_matrix_transposed[j][i];
 }
 
 /**
@@ -42,7 +60,7 @@ static void mix(const Command &cmd, float (&outputs)[4]) {
 
         for (int j=0; j<4; j++) {
             /* calculate new mixed values */
-            outputs[i] += get_b_inv_val(i, j) * inputs[j];
+            outputs[i] += get_motor_matrix_inv_val(i, j) * inputs[j];
         }
     }
 }
@@ -53,12 +71,24 @@ static void mix(const Command &cmd, float (&outputs)[4]) {
  * @param outputs the vector to adapt
  * @param min_out lower limit
  * @param max_out upper limit
+ *
+ * @return true when output was clipped
  */
-static void clip(float (&outputs)[4], float min_out=0.f, float max_out=1.f) {
+static bool clip(float (&outputs)[4], float min_out=0.f, float max_out=1.f) {
+    bool clipping_needed = false;
     for (int i=0; i<4; i++) {
-        if (outputs[i] < min_out) { outputs[i] = min_out; }
-        if (outputs[i] > max_out) { outputs[i] = max_out; }
+        if (outputs[i] < min_out) {
+             outputs[i] = min_out;
+             clipping_needed = true;
+        }
+
+        if (outputs[i] > max_out) {
+            outputs[i] = max_out;
+            clipping_needed = true;
+        }
     }
+    
+    return clipping_needed;
 }
 
 /**
@@ -103,8 +133,10 @@ static float compute_desaturation_gain(const float (&des_vec)[4], float (&output
  * @param outputs the output values to modify
  * @param min_out lower limit, a value in outputs below is saturized
  * @param max_out upper limit, a value in outputs above is saturized
+ * 
+ * @return true when desaturation was needed
  */
-static void desaturate(const float (&des_vec)[4], float (&outputs)[4], float min_out=0.f, float max_out=1.f) {
+static bool desaturate(const float (&des_vec)[4], float (&outputs)[4], float min_out=0.f, float max_out=1.f) {
     float k1 = compute_desaturation_gain(des_vec, outputs, min_out, max_out);
 
     for (int i=0; i<4; i++) {
@@ -116,6 +148,12 @@ static void desaturate(const float (&des_vec)[4], float (&outputs)[4], float min
     for (int i=0; i<4; i++) {
         outputs[i] += 0.5 * k2 * des_vec[i];
     }
+
+    if (k1 == 0.0f && k2 == 0.0f) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 void simple_mix(const Command &cmd, float (&outputs)[4]) {
@@ -126,12 +164,38 @@ void simple_mix(const Command &cmd, float (&outputs)[4]) {
     clip(outputs);
 }
 
-void px_airmode_mix(const Command &cmd, float (&outputs)[4]) {
+enum SATURATION_RESULT px_airmode_mix(const Command &cmd, float (&outputs)[4]) {
+
+    bool was_saturated = false;
+
     mix(cmd, outputs);
 
-    desaturate(thrust_scales, outputs);
+    was_saturated |= desaturate(thrust_scales, outputs);
 
-    desaturate(yaw_scales, outputs);
+    was_saturated |= desaturate(yaw_scales, outputs);
 
-    clip(outputs);
+    was_saturated |= clip(outputs);
+
+    if (was_saturated) {
+        return DESATURATION_PERFORMED;
+    } else {
+        return NO_SATURATION_OCCURED;
+    }
+}
+
+void mixer_get_desired_cmd(Command &cmd, const float (&outputs)[4]) {
+    float desired_inputs[4] = {};
+
+    for (int i=0; i<4; i++) {
+
+        for (int j=0; j<4; j++) {
+            /* calculate new mixed values */
+            desired_inputs[i] += get_motor_matrix_val(i, j) * outputs[j];
+        }
+    }
+
+    cmd.roll = desired_inputs[0];
+    cmd.pitch = desired_inputs[1];
+    cmd.yaw = desired_inputs[2];
+    cmd.thrust = desired_inputs[3];
 }
--- tests/lib/mixer/src/main.cpp
+++ tests/lib/mixer/src/main.cpp
@@ -31,13 +31,22 @@ void test_airmode_mixer() {
 	float outputs[4] = {};
 
 	for (int i=0; i<50; i++) {
-		Command cmd = USE_ONLY_COMMAND(test_data_airmode[i]);
+		const Command cmd = USE_ONLY_COMMAND(test_data_airmode[i]);
+
+		Command cmd_des;
 
 		px_airmode_mix(cmd, outputs);
+
+		/* check back calcuation support on the way */
+		mixer_get_desired_cmd(cmd_des, outputs);
+		float cmd_des_arr[] = {cmd_des.roll, cmd_des.pitch, cmd_des.yaw, cmd_des.thrust};
 
 		for (int j=0; j<4; j++) {
             zassert_within(outputs[j], test_data_airmode[i][4+j], 0.01,
 					"wrong output value (row %d, col %d)", i, j);
+
+			zassert_within(cmd_des_arr[j], test_data_airmode[i][8+j], 0.01,
+					"wrong desaturated value (row %d, col %d)", i, j);
         }
 	}
 }
