152b33b852051cb8763d8bf4d2330cef07292312
Jonathan Hahn <Jonathan.Hahn@t-online.de>
2022-04-01 14:46:03 +0200

    samples: add simple simulator proof of concept

--- samples/sim/src/main.cpp
+++ samples/sim/src/main.cpp
@@ -18,6 +18,7 @@
 
 extern "C" {
     #include <simulator_connection.h>
+    #include <ratecontrol.h>
 }
 #include <custom_drivers/rc.h>
 #include <mixer/mixer.hpp>
@@ -30,10 +31,19 @@ extern "C" {
 
 #define RC_IN DT_NODELABEL(rc)
 
+static void reduce_to_roll_axis(struct Command *rc_in);
+
 void main() {
     const struct device *receiver = DEVICE_DT_GET(RC_IN);
     struct Command rc_in;
+    struct RateControl roll_rate_control;
     float outputs[4];
+    float gyro[3];
+
+    ratecontrol_init(&roll_rate_control, "roll");
+    roll_rate_control.k_p = 0.24f;
+    roll_rate_control.k_i = 0.0f;
+    roll_rate_control.k_d = 0.0f;
 
 	if (!device_is_ready(receiver)) {
 		printk("RC IN device not ready.\n");
@@ -48,15 +58,25 @@ void main() {
     while (true) {
         rc_update(receiver, &rc_in);
 
-        /* run mixer */
+        simulator_update_sensor_values(gyro);
+
+        reduce_to_roll_axis(&rc_in);
+
+        float corrected = ratecontrol_update(&roll_rate_control, rc_in.roll, gyro[0]);
+
+        rc_in.roll = corrected;
+
 		px_airmode_mix(rc_in, outputs);
 
         simulator_send_outputs(outputs);
 
-        printk("out: %d %d %d %d\n", (int)(outputs[0]*100), (int)(outputs[1]*100), (int)(outputs[2]*100), (int)(outputs[3]*100));
-
-        k_sleep(K_MSEC(10));
+        k_sleep(K_MSEC(5));
     }
 
     return;
+}
+
+static void reduce_to_roll_axis(struct Command *rc_in) {
+    rc_in->pitch = 0.0f;
+    rc_in->yaw = 0.0f;
 }
