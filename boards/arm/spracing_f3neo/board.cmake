1b07ac72f3f9384020119c1e444279009c09772c
Jonathan Hahn <helo9@users.noreply.github.com>
2022-09-07 22:00:50 +0200

    board: spracing_f3neo: fix flash and config
    Some options have been taken from cc3d and haven't
    been adapted correctly.

--- boards/arm/spracing_f3neo/board.cmake
+++ boards/arm/spracing_f3neo/board.cmake
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(jlink "--device=STM32F103CB" "--speed=4000")
+board_runner_args(jlink "--device=STM32F303CB" "--speed=4000")
 
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
